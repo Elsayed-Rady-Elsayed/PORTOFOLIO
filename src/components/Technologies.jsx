d3658e4fefeb417b0029a54669f5a11c6501041d
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-17 00:35:10 +0200

    start the project

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -38,7 +38,7 @@ const Technologies = () => {
         className="flex flex-wrap items-center justify-center gap-4"
       >
         <motion.div
-          initial={{ y: -20 }}
+          initial={{ y: -10 }}
           transition={{
             duration: 1.5,
             ease: "linear",
@@ -46,14 +46,14 @@ const Technologies = () => {
             repeatType: "reverse",
           }}
           animate={{
-            y: 20,
+            y: 10,
           }}
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <RiReactjsLine className="text-6xl text-cyan-700" />
         </motion.div>
         <motion.div
-          initial={{ y: -20 }}
+          initial={{ y: -10 }}
           transition={{
             duration: 1.1,
             ease: "linear",
@@ -61,14 +61,14 @@ const Technologies = () => {
             repeatType: "reverse",
           }}
           animate={{
-            y: 20,
+            y: 10,
           }}
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <RiJavascriptLine className="text-6xl text-yellow-400" />
         </motion.div>
         <motion.div
-          initial={{ y: -20 }}
+          initial={{ y: -10 }}
           transition={{
             duration: 1.7,
             ease: "linear",
@@ -76,14 +76,14 @@ const Technologies = () => {
             repeatType: "reverse",
           }}
           animate={{
-            y: 20,
+            y: 10,
           }}
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <RiHtml5Line className="text-6xl text-orange-700" />
         </motion.div>
         <motion.div
-          initial={{ y: -20 }}
+          initial={{ y: -10 }}
           transition={{
             duration: 1.0,
             ease: "linear",
@@ -91,14 +91,14 @@ const Technologies = () => {
             repeatType: "reverse",
           }}
           animate={{
-            y: 20,
+            y: 10,
           }}
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <RiCss3Line className="text-6xl text-blue-700" />
         </motion.div>
         <motion.div
-          initial={{ y: -20 }}
+          initial={{ y: -10 }}
           transition={{
             duration: 2,
             ease: "linear",
@@ -106,14 +106,14 @@ const Technologies = () => {
             repeatType: "reverse",
           }}
           animate={{
-            y: 20,
+            y: 10,
           }}
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <RiNextjsLine className="text-6xl text-neutral-700" />
         </motion.div>
         <motion.div
-          initial={{ y: -20 }}
+          initial={{ y: -10 }}
           transition={{
             duration: 0.7,
             ease: "linear",
@@ -121,14 +121,14 @@ const Technologies = () => {
             repeatType: "reverse",
           }}
           animate={{
-            y: 20,
+            y: 10,
           }}
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <IoLogoSass className="text-6xl text-pink-700" />
         </motion.div>
         <motion.div
-          initial={{ y: -20 }}
+          initial={{ y: -10 }}
           transition={{
             duration: 1.4,
             ease: "linear",
@@ -136,14 +136,14 @@ const Technologies = () => {
             repeatType: "reverse",
           }}
           animate={{
-            y: 20,
+            y: 10,
           }}
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <RiTailwindCssLine className="text-6xl text-teal-500" />
         </motion.div>
         <motion.div
-          initial={{ y: -20 }}
+          initial={{ y: -10 }}
           transition={{
             duration: 1.8,
             ease: "linear",
@@ -151,14 +151,14 @@ const Technologies = () => {
             repeatType: "reverse",
           }}
           animate={{
-            y: 20,
+            y: 10,
           }}
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <BiLogoRedux className="text-6xl text-purple-600" />
         </motion.div>
         <motion.div
-          initial={{ y: -20 }}
+          initial={{ y: -10 }}
           transition={{
             duration: 0.9,
             ease: "linear",
@@ -166,7 +166,7 @@ const Technologies = () => {
             repeatType: "reverse",
           }}
           animate={{
-            y: 20,
+            y: 10,
           }}
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
