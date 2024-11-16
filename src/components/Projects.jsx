30a5ba72a10284d0f9a21f60e981a4f92f3071f5
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-16 23:14:36 +0200

    start the project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,31 +1,34 @@
 import React from "react";
-import { EXPERIENCES } from "../constants";
+import { PROJECTS } from "../constants";
 
-const Experience = () => {
+const Projects = () => {
   return (
-    <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">Experiences</h2>
+    <div className="border-b pb-4 border-neutral-900">
+      <h2 className="my-20 text-center text-4xl">Projects</h2>
       <div>
-        {EXPERIENCES.map((el, idx) => {
+        {PROJECTS.map((el, idx) => {
           return (
             <div key={idx} className="mb-8 flex flex-wrap lg:justify-center">
               <div className="w-full lg:w-1/4">
-                <p className="mb-2 text-sm text-neutral-400">{el.year}</p>
+                <img
+                  src={el.image}
+                  alt=""
+                  width={150}
+                  height={150}
+                  className="mb-6 rounded"
+                />
               </div>
               <div className="w-full max-w-xl lg:w-3/4">
-                <h6 className="mb-2 font-semibold">
-                  {el.role} -{" "}
-                  <span className="text-sm text-purple-100">{el.company}</span>
-                </h6>
+                <h6 className="mb-2 font-semibold">{el.title}</h6>
                 <p className="mb-4 text-neutral-400">{el.description}</p>
                 <div className="flex flex-wrap">
-                  {el.technologies.map((t, idx) => {
+                  {el.technologies.map((el, idx) => {
                     return (
                       <span
                         className="mr-2 mt-4 rounded bg-neutral-900 py-1 px-2 text-sm font-medium text-purple-800"
                         key={idx}
                       >
-                        {t}
+                        {el}
                       </span>
                     );
                   })}
@@ -39,4 +42,4 @@ const Experience = () => {
   );
 };
 
-export default Experience;
+export default Projects;
