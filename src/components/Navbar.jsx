aa87e88e4081537f27c0090d8d6919fe03bdf376
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-12-17 23:06:16 +0200

    edit in my portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,12 @@ const Navbar = () => {
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         {/* <img src={logo} alt="" className="mx-2 w-10" /> */}
-        <p>
+        <a href="">
           <span className="text-5xl">s</span>
           <span className="text-4xl">r</span>
-        </p>
+        </a>
       </div>
-      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+      <div className="md:m-8 flex items-center justify-center gap-4 text-2xl">
         <a
           href="https://www.linkedin.com/in/elsayed-rady-elsayed-73b541288/"
           target="_blank"
